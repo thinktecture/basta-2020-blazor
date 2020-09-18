f4ee1f307107eacb35d6664d22c2729889e9b640
Christian Weyer <christian.weyer@thinktecture.com>
2020-09-18 13:16:01 +0200

    Conferences Master first iteration

--- ConfToolAndMore/ConfToolAndMore/Client/Program.cs
+++ ConfToolAndMore/ConfToolAndMore/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ConfToolAndMore.Client.Services;
 
 namespace ConfToolAndMore.Client
 {
@@ -18,6 +19,8 @@ namespace ConfToolAndMore.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<ConferencesClientService>();
 
             await builder.Build().RunAsync();
         }
---
+++ ConfToolAndMore/ConfToolAndMore/Client/Services/ConferencesClientService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ConfToolAndMore.Shared.DTO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfToolAndMore.Client.Services
+{
+    public class ConferencesClientService
+    {
+        private HttpClient _httpClient;
+        private string _baseApiUrl;
+
+        public ConferencesClientService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _baseApiUrl = config["BaseApiUrl"];
+        }
+
+        public async Task<List<ConferenceOverview>> GetConferencesAsync()
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<ConferenceOverview>>(_baseApiUrl);
+
+            return result;
+        }
+
+        public async Task<ConferenceDetails> GetConferenceDetailsAsync(Guid id)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ConferenceDetails>(_baseApiUrl + id);
+
+            return result;
+        }
+    }
+}
