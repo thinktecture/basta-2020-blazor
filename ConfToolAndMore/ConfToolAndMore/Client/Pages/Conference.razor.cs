173a4bbea8c39fa6ce9aa0f927340e4de7d4e5bb
Christian Weyer <christian.weyer@thinktecture.com>
2020-09-18 14:40:48 +0200

    Conference Details first iteration

---
+++ ConfToolAndMore/ConfToolAndMore/Client/Pages/Conference.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConfToolAndMore.Client.Services;
+using ConfToolAndMore.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+
+namespace ConfToolAndMore.Client.Pages
+{
+    public partial class Conference : ComponentBase
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Parameter]
+        public string Mode { get; set; }
+
+        private bool _isShow { get; set; }
+
+        [Inject]
+        private ConferencesClientService _conferencesClient { get; set; }
+
+        private ConferenceDetails _conferenceDetails = new ConferenceDetails();
+
+        public Conference()
+        {
+            _conferenceDetails = new ConferenceDetails();
+            _conferenceDetails.DateFrom = DateTime.UtcNow;
+            _conferenceDetails.DateTo = DateTime.UtcNow;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _isShow = Mode == ConferenceDetailsModes.Show;
+
+            switch (Mode)
+            {
+                case ConferenceDetailsModes.Show:
+                    var result = await _conferencesClient.GetConferenceDetailsAsync(Id);
+                    _conferenceDetails = result;
+                    break;
+            }
+        }
+    }
+}
---
+++ ConfToolAndMore/ConfToolAndMore/Client/Pages/ConferenceDetailsModes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfToolAndMore.Client.Pages
+{
+    public class ConferenceDetailsModes
+    {
+        public const string Show = "show";
+        public const string Edit = "edit";
+        public const string New = "new";
+    }
+}
